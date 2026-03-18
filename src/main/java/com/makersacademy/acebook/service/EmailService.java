ebbe5dbec0b89b90a763aa76d2eccdd1f7e0fcde
mollyrobbxo <molly.j.r@hotmail.co.uk>
2026-03-18 14:44:20 +0000

    Update EmailService.java

--- src/main/java/com/makersacademy/acebook/service/EmailService.java
+++ src/main/java/com/makersacademy/acebook/service/EmailService.java
@@ -1,9 +1,9 @@
 package com.makersacademy.acebook.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.stereotype.Service;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
@@ -11,12 +11,15 @@ import jakarta.mail.internet.MimeMessage;
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
-
-    public void sendInvite(String toEmail, String groupName, String inviteLink) {
     private final JavaMailSender mailSender;
 
+    // Constructor injection (recommended)
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    // Send HTML invite email
+    public void sendInvite(String toEmail, String groupName, String inviteLink) {
         MimeMessage message = mailSender.createMimeMessage();
 
         try {
@@ -25,28 +28,27 @@ public class EmailService {
             helper.setTo(toEmail);
             helper.setSubject("You're invited to join " + groupName);
 
-            // HTML email content
+            // HTML content
             String htmlMsg = "<p>Hi!</p>"
                     + "<p>You've been invited to join the group '<strong>" + groupName + "</strong>'.</p>"
                     + "<p>Click <a href='" + inviteLink + "'>here</a> to join the group.</p>"
                     + "<p>See you there!</p>";
 
             helper.setText(htmlMsg, true); // true = HTML
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
-
-    public void sendSimpleEmail(String[] to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("🐌 " + subject);
-        message.setText(body);
-        message.setFrom("snailmail@slug.co"); // arbitrary, MailHog will accept it
-        mailSender.send(message);
             mailSender.send(message);
 
         } catch (MessagingException e) {
             e.printStackTrace();
         }
+    }
+
+    // Send simple plain text email (general purpose)
+    public void sendSimpleEmail(String[] to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("🐌 " + subject);
+        message.setText(body);
+        message.setFrom("snailmail@slug.co"); // arbitrary, works for MailHog/Mailtrap
+        mailSender.send(message);
     }
 }
