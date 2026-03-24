0ac193af0fdd89d0e9dc04a25466238894774b9c
mollyrobbxo <molly.j.r@hotmail.co.uk>
2026-03-24 12:00:42 +0000

    Account SetUp

--- src/main/java/com/makersacademy/acebook/controller/FeedController.java
+++ src/main/java/com/makersacademy/acebook/controller/FeedController.java
@@ -89,7 +89,7 @@ public class FeedController {
         LocalDateTime start = cycle.getCycleStart();
         LocalDateTime end = start.plusWeeks(1);
 
-        boolean feedLocked = LocalDateTime.now().isBefore(end);
+        boolean feedLocked = false;//LocalDateTime.now().isBefore(end);
 
         model.addAttribute("feedLocked", feedLocked);
         model.addAttribute("unlockTime", end);
